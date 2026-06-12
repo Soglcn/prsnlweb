ede31fe5db07e347cc49c1279efe0c75ad4dc740
2T2BAC <scyozgat@gmail.com>
2026-06-12 19:54:00 +0300

    120626 - Header buttons added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
+import { Link } from "react-router-dom";
+
 import './App.css'
 import Header from "./assets/compHeader/header.jsx";
-
 function App() {
   return (
     <>
       <Header />
 
       <main>
+        <h1>BIG TEST ITEM</h1>
       </main>
 
     </>
--- src/assets/compHeader/header.jsx
+++ src/assets/compHeader/header.jsx
@@ -1,33 +1,57 @@
+import { Link } from "react-router-dom";
 import "./header.css";
 import headerText from "./header.json";
 import { useLanguage } from "../../context/LanguageContext";
 import { t } from "../../utils/translate";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGlobe } from "@fortawesome/free-solid-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import logo from "../../../public/images/sy_dark_logo.png";
+
 
 function Header() {
     const { language, changeLanguage } = useLanguage();
-console.log(Object.keys(headerText));
+    console.log(Object.keys(headerText));
     return (
         <header className="header">
-            <div className="header__logo">SY</div>
+            <div className="headerLeftCont">
+                <div className="headerLogo">
+                    <Link to="/">
+                        <img src={logo} alt="SY Logo" />
+                    </Link>
+                </div>
+            </div>
 
-            <nav className="header__nav">
+            <nav className="headerNav">
                 <a>{t(headerText, language, "home")}</a>
                 <a>{t(headerText, language, "projects")}</a>
                 <a>{t(headerText, language, "about")}</a>
                 <a>{t(headerText, language, "contact")}</a>
             </nav>
 
-            <select
-                className="header__language"
-                value={language}
-                onChange={(e) => changeLanguage(e.target.value)}
-            >
-                {Object.keys(headerText).map((lang) => (
-                    <option key={lang} value={lang}>
-                        {lang.toUpperCase()}
-                    </option>
-                ))}
-            </select>
+
+            <div className="headerRightCont">
+                <div className="langCont">
+                    <FontAwesomeIcon icon={faGlobe} />
+                    <select
+                        className="headerLang"
+                        value={language}
+                        onChange={(e) => changeLanguage(e.target.value)}
+                    >
+                        {Object.keys(headerText).map((lang) => (
+                            <option key={lang} value={lang}>
+                                {lang.toUpperCase()}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+                <div className="burgerCont">
+                    <button className="btnBurger">
+                        <FontAwesomeIcon icon={faBars} />
+                    </button>
+                </div>
+            </div>
+
         </header>
     );
 }
