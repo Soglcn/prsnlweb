212e2a02ba8c442fd61a8f93931a740debc50aa7
2T2BAC <scyozgat@gmail.com>
2026-06-13 14:22:14 +0300

    130626 - Featured projects container.

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { useLanguage } from "./context/LanguageContext";
 import { t } from "./utils/translate";
 
 import HeroModel from "./assets/HeroModel";
+import compassIcon from "/images/compass.png";
 
 function App() {
   const { language } = useLanguage();
@@ -35,6 +36,42 @@ function App() {
               src="/images/contallienbg.png"
               alt=""
             />
+          </div>
+        </div>
+
+        <div className="selectedProjects">
+          <div className="selTitle">
+            <h3 className="selSub">{t(appText, language, "selectedWorks")}</h3>
+            <div className="compassLine">
+              <img src={compassIcon} alt="Compass" />
+              <span></span>
+            </div>
+          </div>
+
+          <div className="featured">
+            <div className="featuredText">
+              <h3 className="featureNumber">01</h3>
+              <h1 className="featuredName">XENO</h1>
+              <h2 className="featuredDesc">{t(appText, language, "xenoDesc")} </h2>
+              <button className="btnGoProject">{t(appText, language, "goProject")} <span></span><span className="arrowUpRight">→</span></button>
+            </div>
+          </div>
+          <div className="featured">
+            <div className="featuredText">
+              <h3 className="featureNumber">02</h3>
+              <h1 className="featuredName">MEOWDITATION</h1>
+              <h2 className="featuredDesc">{t(appText, language, "meowDesc")} </h2>
+              <button className="btnGoProject">{t(appText, language, "goProject")} <span></span><span className="arrowUpRight">→</span></button>
+
+            </div>
+          </div>
+          <div className="featured">
+            <div className="featuredText">
+              <h3 className="featureNumber">03</h3>
+              <h1 className="featuredName">COSMIC CONTAMINATION</h1>
+              <h2 className="featuredDesc">{t(appText, language, "cosmicDesc")} </h2>
+              <button className="btnGoProject">{t(appText, language, "goProject")} <span></span><span className="arrowUpRight">→</span></button>
+            </div>
           </div>
         </div>
 
