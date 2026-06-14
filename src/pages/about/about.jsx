b221d2b99ca70787c450dd13b3b879cecc0b2e78
2T2BAC <scyozgat@gmail.com>
2026-06-14 16:19:31 +0300

    140626 - about page.

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -8,7 +8,34 @@ function About() {
     const { language } = useLanguage();
     return (
         <main className="aboutPage">
-            boout
+            <div className="abHead">
+                <h2 className="abTopTitle">{t(abt, language, "title")}</h2>
+                <div className="abContext">
+                    <img src="/images/Heykel_2.png" alt="" />
+                    <p className="abCatch">{t(abt, language, "aboutCatch")}</p>
+                </div>
+            </div>
+
+            <div className="selectedProjects">
+                <div className="selTitle">
+                    <div className="compassLine">
+                        <span></span>
+                        <img src="/images/compass.png" alt="Compass" />
+                        <span></span>
+                    </div>
+                </div>
+            </div>
+
+            <div className="abHistory">
+                <h2 className="abTopTitle">{t(abt, language, "journey")}</h2>
+                <p>{t(abt, language, "bio_1")}</p>
+                <br />
+                <p>{t(abt, language, "bio_2")}</p>
+                <br />
+                <p>{t(abt, language, "bio_3")}</p>
+                <br />
+                <p>{t(abt, language, "bio_4")}</p>
+            </div>
         </main>
     );
 }
