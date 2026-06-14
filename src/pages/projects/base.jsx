3807403d0f481059766c5d4b85fcbf7fa96295a0
2T2BAC <scyozgat@gmail.com>
2026-06-15 00:59:01 +0300

    150626 - Project details page base has created.

--- src/pages/projects/base.jsx
+++ src/pages/projects/base.jsx
@@ -21,81 +21,87 @@ function Xeno() {
 
     return (
         <main className="projectPage">
-            <Link to="/projects" className="backButton">{t(pt, language, "back")}</Link>
-            <img src="/images/projects/xeno.png" alt="" />
 
-            <h1>{t(pt, language, "title")}</h1>
-            <p>{t(pt, language, "subtitle")}</p>
+            <div className="base">
+                <Link to="/projects" className="backButton">{t(pt, language, "back")}</Link>
+                <img src="/images/projects/xeno.png" alt="" />
 
-            <div className="stStat">
-                <FontAwesomeIcon icon={faCircleCheck} />
-                <h1>{t(pt, language, "stStat")}</h1>
-                <h2>{t(pt, language, "stActive")}</h2>
-            </div>
+                <div className="headAr">
+                    <h1>{t(pt, language, "title")}</h1>
+                    <p>{t(pt, language, "subtitle")}</p>
+                </div>
 
-            <div className="stRole">
-                <FontAwesomeIcon icon={faUserTie} />
-                <h1>{t(pt, language, "stRole")}</h1>
-                <h2>{t(pt, language, "stRoleExp")}</h2>
-            </div>
+                <div className="statAr">
+                    <div className="stStat">
+                        <FontAwesomeIcon icon={faCircleCheck} />
+                        <h1>{t(pt, language, "stStat")}</h1>
+                        <h2>{t(pt, language, "stActive")}</h2>
+                    </div>
 
-            <div className="stTech">
-                <FontAwesomeIcon icon={faMicrochip} />
-                <h1>{t(pt, language, "stTech")}</h1>
-                <h2>{t(pt, language, "stTechExp")}</h2>
-            </div>
+                    <div className="stRole">
+                        <FontAwesomeIcon icon={faUserTie} />
+                        <h1>{t(pt, language, "stRole")}</h1>
+                        <h2>{t(pt, language, "stRoleExp")}</h2>
+                    </div>
 
-            <div className="stYear">
-                <FontAwesomeIcon icon={faCalendarDays} />
-                <h1>{t(pt, language, "stYear")}</h1>
-                <h2>{t(pt, language, "stYearExp")}</h2>
-            </div>
+                    <div className="stTech">
+                        <FontAwesomeIcon icon={faMicrochip} />
+                        <h1>{t(pt, language, "stTech")}</h1>
+                        <h2>{t(pt, language, "stTechExp")}</h2>
+                    </div>
 
-            <div className="prAb">
-                <div className="selectedProjects">
-                    <div className="selTitle">
-                        <div className="compassLine">
-                            <span></span>
-                            <img src="/images/compass.png" alt="Compass" />
-                            <span></span>
+                    <div className="stYear">
+                        <FontAwesomeIcon icon={faCalendarDays} />
+                        <h1>{t(pt, language, "stYear")}</h1>
+                        <h2>{t(pt, language, "stYearExp")}</h2>
+                    </div>
+                </div>
+
+                <div className="prAb">
+                    <div className="selectedProjects">
+                        <div className="selTitle">
+                            <div className="compassLine">
+                                <span></span>
+                                <img src="/images/compass.png" alt="Compass" />
+                                <span></span>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div className="over">
-                <h1>{t(pt, language, "overview")}</h1>
-                <p>{t(pt, language, "prExp")}</p>
-            </div>
+                <div className="over">
+                    <h1>{t(pt, language, "overview")}</h1>
+                    <p>{t(pt, language, "prExp")}</p>
+                </div>
 
-            <div className="prAb">
-                <div className="selectedProjects">
-                    <div className="selTitle">
-                        <div className="compassLine">
-                            <span></span>
-                            <img src="/images/compass.png" alt="Compass" />
-                            <span></span>
+                <div className="prAb">
+                    <div className="selectedProjects">
+                        <div className="selTitle">
+                            <div className="compassLine">
+                                <span></span>
+                                <img src="/images/compass.png" alt="Compass" />
+                                <span></span>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div className="usedTechs">
-                <h1>{t(pt, language, "techs")}</h1>
-                <div className="uTech">
-                    <div className="techLabel"> Unity </div>
+                <div className="usedTechs">
+                    <h1>{t(pt, language, "techs")}</h1>
+                    <div className="uTech">
+                        <div className="techLabel"> Unity </div>
+                    </div>
+                </div>
+
+                <div className="moreDetail">
+                    <img src="" alt="" className="goddess" />
+                    <div className="detTextArea">
+                        <h1>{t(pt, language, "wantMore")}</h1>
+                        <p>{t(pt, language, "wantContact")}</p>
+                        <button className="btnCont">{t(pt, language, "getContact")}<span></span><span className="arrowUpRight">→</span></button>
+                    </div>
                 </div>
             </div>
-
-            <div className="moreDetail">
-                <img src="" alt="" className="goddess" />
-                <div className="detTextArea">
-                    <h1>{t(pt, language, "wantMore")}</h1>
-                    <p>{t(pt, language, "wantContact")}</p>
-                    <button className="btnCont">{t(pt, language, "getContact")}<span></span><span className="arrowUpRight">→</span></button>
-                </div>
-            </div>
-
             <div className="footerMap"><Footer /></div>
 
         </main>
